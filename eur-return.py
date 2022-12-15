c5977f9c527c57bc338dbd8c1b7bb8daec13f2e8
Sven Azari <94923703+svenazari@users.noreply.github.com>
2022-12-15 18:50:55 +0100

    Add files via upload

--- eur-return.py
+++ eur-return.py
@@ -33,27 +33,36 @@ while True:
         window['izncon'].update('')
     elif event == "EUR to HRK":
         hrk = values['izn'] #iznso u hrk
-        hrkf = float(hrk.replace(',','.')) #float hrk
-        eur = round(hrkf * 7.53450,2)
-        eurp = str(eur) + " HRK"
-        window['izncon'].update(eurp)
+        if hrk == "":
+            pass
+        else:
+            hrkf = float(hrk.replace(',','.')) #float hrk
+            eur = round(hrkf * 7.53450,2)
+            eurp = str(eur) + " HRK"
+            window['izncon'].update(eurp)
     elif event == "HRK to EUR":
         eur = values['izn'] #cijena u eur
-        eurf = float(eur.replace(',','.')) #float eur
-        kn = round(eurf / 7.53450,2)
-        knp = str(kn) + " EUR"
-        window['izncon'].update(knp)
+        if eur == "":
+            pass
+        else:
+            eurf = float(eur.replace(',','.')) #float eur
+            kn = round(eurf / 7.53450,2)
+            knp = str(kn) + " EUR"
+            window['izncon'].update(knp)
     elif event == "IZRAČUNAJ":
         eur = values['eur'] #cijena
-        eurf = float(eur.replace(',','.')) #float eur
-        hrk = values['hrk'] #plaćeni iznos
-        hrkf = float(hrk.replace(',','.')) #float hrk
-        rate = 7.53450 #fiksni tečaj za konverziju
+        if eur == "":
+            pass
+        else:
+            eurf = float(eur.replace(',','.')) #float eur
+            hrk = values['hrk'] #plaćeni iznos
+            hrkf = float(hrk.replace(',','.')) #float hrk
+            rate = 7.53450 #fiksni tečaj za konverziju
         #računanje
-        hrk2eur = round(hrkf / rate,2) #pretvaranje kuna u euro
-        raz = round(hrk2eur - eurf,2) #računanje razlike
-        razp = "Za vratiti " + str(raz) + " EUR"
-        window['back'].update(razp)
+            hrk2eur = round(hrkf / rate,2) #pretvaranje kuna u euro
+            raz = round(hrk2eur - eurf,2) #računanje razlike
+            razp = "Za vratiti " + str(raz) + " EUR"
+            window['back'].update(razp)
 
 #verzija 3: preračunavanje iz kuna u euro i obratno u gornjem dijelu prozora
 #verzija 2: dodana mogućnost da se cijena u EUR preračuna u HRK
