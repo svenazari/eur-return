88356e7ec20c4332548074be41d80b506d35b2c0
Sven Azari <94923703+svenazari@users.noreply.github.com>
2022-12-16 18:25:15 +0100

    Add files via upload

--- eur-return.py
+++ eur-return.py
@@ -13,7 +13,7 @@ layout = [  [sg.Text("Iznos"), sg.InputText(size=(6,1), key='izn')],
             [sg.Text("", text_color='yellow', key='izncon')],
             [sg.Text("Cijena EUR"), sg.InputText(size=(6,1), key='eur')], #unos cijene u eurima
             [sg.Text("Plaćeno HRK"), sg.InputText(size=(6,1), key='hrk')], #unos plaćenog u kunama
-            [sg.Text("Za vratiti", text_color='yellow', key='back')], #mjesto za ispis izračuna
+            [sg.Text("", text_color='yellow', key='back')], #mjesto za ispis izračuna
             [sg.Button("IZRAČUNAJ")], 
             [sg.Button("NOVO")], 
             [sg.Button("EXIT")]
@@ -28,7 +28,7 @@ while True:
     elif event == "NOVO":
         window['eur'].update('')
         window['hrk'].update('')
-        window['back'].update('Za vratiti')
+        window['back'].update('')
         window['izn'].update('')
         window['izncon'].update('')
     elif event == "EUR to HRK":
@@ -58,12 +58,18 @@ while True:
             hrk = values['hrk'] #plaćeni iznos
             hrkf = float(hrk.replace(',','.')) #float hrk
             rate = 7.53450 #fiksni tečaj za konverziju
-        #računanje
+            #računanje
             hrk2eur = round(hrkf / rate,2) #pretvaranje kuna u euro
             raz = round(hrk2eur - eurf,2) #računanje razlike
-            razp = "Za vratiti " + str(raz) + " EUR"
+            if raz > 0:
+                razp = "Za vratiti " + str(raz) + " EUR"
+            elif raz < 0:
+                razz = raz * -1
+                razzkn = round(razz * 7.53450,2)
+                razp = "Fali " + str(razzkn) + " HRK (" + str(razz) + " EUR)"
             window['back'].update(razp)
 
+#verzija 4: skripta više ne pokazuje minus ako je iznos u kunama manji od potrebnog u eurima, već javlja koliko kuna (eura) fali
 #verzija 3: preračunavanje iz kuna u euro i obratno u gornjem dijelu prozora
 #verzija 2: dodana mogućnost da se cijena u EUR preračuna u HRK
 #verzija 1: osnovno preračunavanje i izračuna razlike u eurima
